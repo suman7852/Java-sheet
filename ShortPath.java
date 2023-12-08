4fd41a22056909e78f31ebddacd53cf0fd9b2978
Suman Singh <sumansinghdbg0475@gmail.com>
2023-12-08 20:12:01 +0530

    ShortestPath.java

---
+++ ShortPath.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class ShortPath {
+    public static double ShortestPath(String path){
+        int x = 0, y=0;
+        for(int i=0; i<path.length(); i++){
+            char dir = path.charAt(i);
+            if(dir == 'N' ){
+                y++;
+            }else if(dir == 'S'){
+                y--;
+            }
+            else if(dir =='E'){
+                x++;
+            }
+            else{
+                x--;
+            }
+        }
+        int x2 = x*x;
+        int y2 = y*y;
+        return Math.sqrt(x2+y2);
+    }
+    public static void main(String[] args) {
+        Scanner sc =  new Scanner(System.in);
+        String path = sc.nextLine();
+        System.out.println(ShortestPath(path));
+    }
+}
+
+
+
+
