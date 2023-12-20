65235f546ff68fecfe5362e3a298bada3b5ebae6
Suman Singh <sumansinghdbg0475@gmail.com>
2023-12-20 18:01:08 +0530

    clear Ith bit

---
+++ bit5.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class bit5 {
+    public static int clearIthbit(int  n, int i){
+        int bitMask = ~(1<<i);
+        return n & bitMask;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int i = sc.nextInt();
+        System.out.println(clearIthbit(n, i));
+    }
+}
